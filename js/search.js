669b2afe3d39730aa352e9d8f0bf7980acd70e24
Arfurboy <arfurboy@gmail.com>
2015-02-15 13:47:40 -0800

    (refactor) improves highlighting feature

--- js/search.js
+++ js/search.js
@@ -6,7 +6,7 @@ function search(){
   if (inputText.length > 0) {
     document.getElementById("wordCount").innerHTML = getCountOccurances(searchText, inputText);
     document.getElementById("query").innerHTML = inputText;
-    highlightSingleWordOccurances(searchText, inputText);
+    highlightOccurances(searchText, inputText);
   }
   return false;
 };
@@ -19,11 +19,13 @@ function search(){
 function getCountOccurances(element, string){
   var words = element.textContent;
   var query = escapeRegExp(string);
-  if (/\w/.test(query[query.length-1])) {
-    query += "\\b";
-  }
+
+  if (/\w/.test(query[query.length-1])) query += "\\b";
+  if (/\w/.test(query[0])) query = "\\b" + query  ;
+
   var regExp = new RegExp(query, "gi");
   var matches = words.match(regExp);
+
   if (matches) return matches.length;
   else return 0;
 };
@@ -31,18 +33,40 @@ function getCountOccurances(element, string){
 /**
 * input:  element
           string  (one word)
-* output: highlights occurances
+* output: highlights occurances or string in element's text
 */
-var originalSearchText = document.getElementById("search_text").innerHTML;
-function highlightSingleWordOccurances(element, string){
-  var words = element.innerHTML = originalSearchText;
+var originalSearchHTML = document.getElementById("search_text").innerHTML;
+function highlightOccurances(element, string){
+  var words = element.innerHTML = originalSearchHTML;
   var query = escapeRegExp(string);
 
-  var regExp = new RegExp(query, "gi");
-  if ((/^\s*\b\w+\b\s*$/).test(query)) {
-    words = words.replace(regExp, "<span class=\"highlight\">$&</span>");
-    element.innerHTML = words;
+  // remove begining and ending white spaces
+  query = query.replace(/^\s+/,"");
+  query = query.replace(/\s+$/,"");
+  // add word boundary restrictions
+  if (/\w/.test(query[query.length-1])) query += "\\b";
+  if (/\w/.test(query[0])) query = "\\b" + query  ;
+  // split query by white spaces
+  query = query.split(/\s+/);
+  // build regular expression
+  var regularExpressionString = "(>[^</]*)(" + query[0] + ")";
+  var replacement = "$1<span class=\"highlight\">$2</span>";
+  var counter = 2;
+  for(var i=1; i<query.length; i++){
+    regularExpressionString += "((?:\\s*<[^>]*>\\s*)+|\\s+)";
+    regularExpressionString += "(" + query[i] + ")";
   }
+  var regExp = new RegExp(regularExpressionString, "gi");
+  for(var i=1; i<query.length; i++){
+    replacement += "$" + ++counter;
+    replacement += "<span class=\"highlight\">$"+ ++counter +"</span>";
+  }
+  // console.log(regExp);
+  // console.log(replacement);
+  var matches = element.innerHTML.match(regExp);
+  element.innerHTML = element.innerHTML.replace(regExp, replacement);
+  if (matches) return matches.length;
+  else return 0;
 };
 
 /**
