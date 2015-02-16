d8f2c28cf9e36ab435b41e8fe82971a0171d0631
Arfurboy <arfurboy@gmail.com>
2015-02-15 20:20:06 -0800

    (refactor) changes the tabbed content view

--- js/tabbed_content.js
+++ js/tabbed_content.js
@@ -2,15 +2,20 @@ var tabContents = document.getElementsByClassName("tabContent");
 var currentTab = 1;
 
 // hide all but tab 1 on initial page load
-for(var i=1; i<tabContents.length; i++){
-  tabContents[i].style.display = "none";
-}
+// for(var i=1; i<tabContents.length; i++){
+//   tabContents[i].style.display = "none";
+// }
 
 /**
 * input: tab to swap to (by number)
-* output: swaps to specified tab
+* output: swaps to specified tab & highlights button
 */
 function swapTo(tabNum){
+  // highlight button
+  var buttons = document.getElementsByTagName("button");
+  buttons[currentTab-1].classList.remove("selected");
+  buttons[tabNum-1].classList.add("selected");
+  // swap to tab
   tabContents[currentTab-1].style.display = "none";
   tabContents[tabNum-1].style.display = "";
   currentTab = tabNum;
