f2410739a1eb65f5c6401e15b4ccceb30e280801
Arfurboy <arfurboy@gmail.com>
2015-02-15 22:06:06 -0800

    (feat) adds slide show to tabbed content

--- js/tabbed_content.js
+++ js/tabbed_content.js
@@ -1,22 +1,77 @@
+var SWAP_INTERVAL = 8000;
+var FADE_DURATION = 1500;
+
 var tabContents = document.getElementsByClassName("tabContent");
 var currentTab = 1;
+var imagePaths = [
+["../img/arena_art/gi-modes-sr-jungle.jpg", "../img/arena_art/gi-modes-sr-baron.jpg", "../img/arena_art/gi-modes-sr-dragon.jpg", "../img/arena_art/gi-modes-sr-battle.jpg"],
+["../img/arena_art/gi-modes-tt-altars.jpg", "../img/arena_art/gi-modes-tt-battle.jpg", "../img/arena_art/gi-modes-tt-jungle.jpg", "../img/arena_art/gi-modes-tt-vilemaw.jpg"],
+["../img/arena_art/gi-modes-ha-basereturn.jpg", "../img/arena_art/gi-modes-ha-battle.jpg", "../img/arena_art/gi-modes-ha-inhibitor.jpg", "../img/arena_art/gi-modes-ha-relics.jpg"],
+["../img/arena_art/gi-modes-cs-capturing.jpg", "../img/arena_art/gi-modes-cs-choosing.jpg", "../img/arena_art/gi-modes-cs-health.jpg", "../img/arena_art/gi-modes-cs-quest.jpg", "../img/arena_art/gi-modes-cs-speed.jpg", "../img/arena_art/gi-modes-cs-storm.jpg"]
+];
+var imageRotator = [];
+for(var i=0; i<imagePaths.length; i++){
+  imageRotator[i] = rotatorFactory(imagePaths[i]);
+}
+var interval, swapTo;
 
-// hide all but tab 1 on initial page load
-// for(var i=1; i<tabContents.length; i++){
-//   tabContents[i].style.display = "none";
-// }
+
+(function loadScript(callback){
+
+  // Load animations script then build search functionality
+  var script = document.createElement("script");
+  script.type = "text/javascript"
+  script.src = "../js/libs/animations.js";
+  script.onload = callback;
+  document.head.appendChild(script);
+
+})(function(){
+
+  // initiate first picture & start slide show interval
+  var picture = document.getElementById("picture");
+  picture.src = imagePaths[0][0];
+  interval = setInterval(function(){
+    fadeOut(picture, FADE_DURATION, function(){
+      picture.src = imageRotator[currentTab-1]();
+      fadeIn(picture, FADE_DURATION);
+    });
+  }, SWAP_INTERVAL);
+
+  /**
+  * input: tab to swap to (by number)
+  * output: swaps to specified tab & highlights button
+  */
+  swapTo = function(tabNum){
+    // highlight button
+    var buttons = document.getElementsByTagName("button");
+    buttons[currentTab-1].classList.remove("selected");
+    buttons[tabNum-1].classList.add("selected");
+    // swap to tab
+    tabContents[currentTab-1].style.display = "none";
+    tabContents[tabNum-1].style.display = "";
+    currentTab = tabNum;
+    // swap picture
+    // var picture = document.getElementById("picture");
+    picture.src = imageRotator[currentTab-1]();
+    clearInterval(interval);
+    interval = setInterval(function(){
+      fadeOut(picture, FADE_DURATION, function(){
+        picture.src = imageRotator[currentTab-1]();
+        fadeIn(picture, FADE_DURATION);
+      });
+    }, SWAP_INTERVAL);
+  };
+});
 
 /**
-* input: tab to swap to (by number)
-* output: swaps to specified tab & highlights button
+* input:  array
+* output: looping iterator for given array
 */
-function swapTo(tabNum){
-  // highlight button
-  var buttons = document.getElementsByTagName("button");
-  buttons[currentTab-1].classList.remove("selected");
-  buttons[tabNum-1].classList.add("selected");
-  // swap to tab
-  tabContents[currentTab-1].style.display = "none";
-  tabContents[tabNum-1].style.display = "";
-  currentTab = tabNum;
-}
+function rotatorFactory(list){
+  var index = 1;
+  var length = list.length;
+  return function(){
+    if (index >= length) index = 0;
+    return list[index++];
+  };
+};
